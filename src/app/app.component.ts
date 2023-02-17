b39b79876e6a6da97477974b6c3fdd2bf1b2d0ee
gosai09 <gosai09@gmail.com>
2023-02-17 16:07:16 -0500

    card is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'card-project';
+export class AppComponent implements OnInit  {
+
+  posts = [
+    {
+      title:'image1',
+      username:'bhadresh gosai',
+      imageUrl:'assets/images/img (1).jpg',
+      describe:'hi! this is image1 bhadresh gosai'
+    },
+    
+    {
+      title:'image2',
+      username:'ramesh gosai',
+      imageUrl:'assets/images/img (2).jpg',
+      describe:'hi! this is image1 ramesh gosai'
+    },
+    
+    {
+      title:'image3',
+      username:'darshan gosai',
+      imageUrl:'assets/images/img (4).jpg',
+      describe:'hi! this is image1 darshan gosai'
+    },
+    {
+      title:'image4',
+      username:'radhe gosai',
+      imageUrl:'assets/images/img (3).jpg',
+      describe:'hi! this is image1 radhe gosai'
+    }
+  ];
+
+  ngOnInit(): void {
+
+  }
+
 }
